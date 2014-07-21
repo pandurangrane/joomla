e714633a69b8314927d473b8ce2086c8fced8eee
Sneha Kahane <sneha_k@tekdi.net>
2014-07-21 20:09:24 +0530

    test commit pandurang

--- 2checkout/2checkout/helper.php
+++ 2checkout/2checkout/helper.php
@@ -3,6 +3,7 @@
  *  @copyright  Copyright (c) 2009-2013 TechJoomla. All rights reserved.
  *  @license    GNU General Public License version 2, or later
  */
+ 
 defined( '_JEXEC' ) or die( ';)' );
 	jimport('joomla.html.html');
 	jimport( 'joomla.plugin.helper' );
